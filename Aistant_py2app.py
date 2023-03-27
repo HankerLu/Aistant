f109d4315558d2811a1d95b71cbbf10d6136b65e
HankerLu <437348250@qq.com>
2023-03-27 22:18:51 +0800

    NEW: update py2app

--- Aistant_py2app.py
+++ Aistant_py2app.py
@@ -3,12 +3,12 @@ from setuptools import setup
 #写一个基于py2app的setup.py文件，用于打包Aisttant_UI_agent.py文件，依赖openai和PyQt5, 并且将图标文件一起打包 
 setup(
     app=['Aistant_UI_agent.py'],
-    data_files=['8666753_message_circle_chat_icon.icns'],
+    data_files=['216180_text_document_icon.icns'],
     options={
         'py2app': {
-            'iconfile': '8666753_message_circle_chat_icon.icns',
+            'iconfile': '216180_text_document_icon.icns',
             'argv_emulation': True,
-            'includes': ['openai', 'PyQt5'],
+            'includes': ['openai', 'PyQt5', 'json', 'time', 'os', 'sys', 'threading']
         }
     },
     setup_requires=['py2app'],
